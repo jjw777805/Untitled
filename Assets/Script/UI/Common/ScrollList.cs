91287f6865162ba21cfc3a7465ada7202b2e12ab
jjw777805 <2033009449@qq.com>
2026-08-10 00:19:46 +0800

    初步实现了自动调节的滚动列表的实现

--- Assets/Script/UI/Common/ScrollList.cs
+++ Assets/Script/UI/Common/ScrollList.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MyUI
 {
@@ -8,23 +11,61 @@ namespace MyUI
     /// </summary>
     public class ScrollList:MonoBehaviour
     {
+        [SerializeField]
         protected int count  = 1;
         protected List<GameObject>item=new List<GameObject>();
         public GameObject templateGO;
+        protected int existCount=0;
+        protected int maxCount=0;
+        public float heightSpace = 0;
+        protected float height = 0;
+        protected float upDelta = 0; 
+        protected RectTransform rect;
+        public RectTransform layoutSize;
+        
         public void SetCount(int n)
         {
             count = n;
         }
 
+        void RectInitialize()
+        {
+            rect = GetComponent<RectTransform>();
+            rect.pivot=new Vector2(0.5f,1);
+            rect.anchorMin = new Vector2(0.5f, 1f);
+            rect.anchorMax = new Vector2(0.5f, 1f);
+            rect.anchoredPosition3D = Vector3.zero;
+        }
+
+        protected void ClearList()
+        {
+            foreach(var i in item){
+                Destroy(i);
+            }
+            item.Clear();
+        }
         public virtual void Initialized()
         {
+            ClearList();
+            RectInitialize();
             if(templateGO == null)
             {
                 Debug.LogError("no samples");
                 return ;
             }
+            height = templateGO.GetComponent<RectTransform>().rect.height;
+            float tHeight = layoutSize.rect.height;
+            maxCount = existCount = 1 + (int)math.ceil((tHeight - height )/(height+heightSpace));
+            upDelta = (maxCount-1)*(height+heightSpace) + height - tHeight;
+            // Debug.Log("The Max Count is " + maxCount);
+
+            if(existCount > count)existCount = count;
+            else existCount = existCount + 1;
+
+            // Debug.Log("The Exist Count is " + existCount);
+
             templateGO.SetActive(false);
-            for(int i = 0; i < count; i++)
+            for(int i = 0; i < existCount; i++)
             {
                 GameObject t = Instantiate(templateGO);
                 item.Add(t);
--- Assets/Script/UI/Panel/CollectionPanel/CltScrollList.cs
+++ Assets/Script/UI/Panel/CollectionPanel/CltScrollList.cs
@@ -12,6 +12,8 @@ namespace MyUI
 
         public CltDiscription cDiscription;
         public NavigateButton navigationButton;
+        bool isSelect;
+        int currentSelectedID = -1;
 
         public void SetData(List<CollectionCommonData> it)
         {
@@ -22,43 +24,100 @@ namespace MyUI
         {
             itemImage = it;
         }
+
+        int head = 0 , tail;
+        void InitailList()
+        {
+            head=0;
+            tail = existCount;
+
+        }
+
+        void DealWithDown()
+        {
+            //如果能够显示的数量大于需要缓冲的，直接忽略
+            // Debug.Log("before:"+head+","+tail+","+currentSelectedID);
+            if(maxCount>existCount)return;
+            currentSelectedID ++;
+            // Debug.Log("now:"+head+","+tail+","+currentSelectedID);
+            int index = currentSelectedID;
+            if(index == tail - 1)
+            {
+                if(maxCount == existCount)
+                {
+                    rect.anchoredPosition = new Vector2(0,upDelta);
+                }
+                else HeadToTail();
+                
+            }
+            else if( maxCount != existCount && index == tail - 2)
+            {
+                //tail - 1是空出来做缓冲的
+                rect.anchoredPosition = new Vector2(0,upDelta);
+            }
+            item[index%existCount].GetComponent<Selectable>().Select();
+        }
+        
+        void HeadToTail()
+        {
+            int index = head%existCount;
+            var it = item[index];
+            ButtonInitial(index,tail);
+            it.GetComponent<RectTransform>().SetAsLastSibling();
+            head++;
+            tail++;
+        }
+        void ButtonInitial(int itemIndex,int dataIndex)
+        {
+            itemIndex %= existCount;
+            var it = item[itemIndex];
+            var text = it.GetComponentInChildren<TMP_Text>();
+            var bt = it.GetComponent<Button>();
+            // Debug.Log(((text)==null) +" "+ (bt==null));
+            int trueDataIndex = dataIndex % itemData.Count;
+
+            text.SetText(itemData[trueDataIndex].objName);
+            bt.onClick.RemoveAllListeners();
+            bt.onClick.AddListener(
+                () =>
+                {
+                    UIManager.instance.OpenMsgBox(
+                        GetInstanceID()+"_"+trueDataIndex,
+                        itemData[trueDataIndex].detailDiscription,
+                        new Vector2(1600,800)
+                    );
+                }
+            );
+            bt.onSelect.RemoveAllListeners();
+            bt.onSelect.AddListener(
+                () =>
+                {
+                    isSelect = true;
+                    currentSelectedID = dataIndex;
+                    cDiscription.SetImage(itemImage[trueDataIndex]);
+                    cDiscription.SetBriefText(itemData[trueDataIndex].shortDiscription);
+                }
+            );
+            bt.OnExit.RemoveAllListeners();
+            bt.OnExit.AddListener(
+                () =>
+                {
+                    isSelect = false;
+                    currentSelectedID = -1;
+                    navigationButton.Select();
+                    navigationButton.SetIsIn(false);
+                }
+            );
+        }
+        [ContextMenu("Initial")]
         public override void Initialized()
         {
             base.Initialized();
-            for(int i=0;i<count;i++)
+            InitailList();
+            isSelect = false ;
+            for(int i=0;i<existCount;i++)
             {
-                var it = item[i];
-                var text = it.GetComponentInChildren<TMP_Text>();
-                var bt = it.GetComponent<Button>();
-                // Debug.Log(((text)==null) +" "+ (bt==null));
-                text.SetText(itemData[i].objName);
-
-                int index = i;
-
-                bt.onClick.AddListener(
-                    () =>
-                    {
-                        UIManager.instance.OpenMsgBox(
-                            GetInstanceID()+"_"+index,
-                            itemData[index].detailDiscription,
-                            new Vector2(1600,800)
-                        );
-                    }
-                );
-                bt.onSelect.AddListener(
-                    () =>
-                    {
-                        cDiscription.SetImage(itemImage[index]);
-                        cDiscription.SetBriefText(itemData[index].shortDiscription);
-                    }
-                );
-                bt.OnExit.AddListener(
-                    () =>
-                    {
-                        navigationButton.Select();
-                        navigationButton.SetIsIn(false);
-                    }
-                );
+               ButtonInitial(i,i);
             }
         }
 
@@ -71,6 +130,28 @@ namespace MyUI
             }
             var bt = item[0].GetComponent<Button>();
             bt.Select();
+        }
+        private MyInput inputs;
+        static float beginTime;
+        public void Start()
+        {
+            inputs=GameManager.instance.GetInputs();
+            beginTime=Time.realtimeSinceStartup;
+            
+        }
+        public void Update()
+        {
+            float delta = Time.realtimeSinceStartup - beginTime;
+            if(delta < 0.2f )return ;
+            if (inputs.Player.Move.IsPressed() && isSelect )
+            {   
+                Vector2 move = inputs.Player.Move.ReadValue<Vector2>();
+                
+                if (move.y > 0.1f) ;
+                else if (move.y < -0.1f) DealWithDown();
+
+                beginTime = Time.realtimeSinceStartup;
+            }
         }
     }
 }
