efc37bb74280483fed0022747c4c8ea5d995263b
jjw777805 <2033009449@qq.com>
2026-08-10 13:26:56 +0800

    修复：
    新增：
    1.滚动列表的真正实现
    
    待办：

--- Assets/Script/UI/Panel/CollectionPanel/CltScrollList.cs
+++ Assets/Script/UI/Panel/CollectionPanel/CltScrollList.cs
@@ -33,14 +33,39 @@ namespace MyUI
 
         }
 
+        void DealWithUp()
+        {
+            //如果能够显示的数量大于需要缓冲的，直接忽略
+            // Debug.Log("before:"+head+","+tail+","+currentSelectedID);
+            if(maxCount>existCount || currentSelectedID == 0)return;
+            currentSelectedID --;
+            // Debug.Log("now:"+head+","+tail+","+currentSelectedID);
+            int index = currentSelectedID;
+            if(index == head - 1)
+            {
+                if(maxCount == existCount)
+                {
+                    rect.anchoredPosition = new Vector2(0,0);
+                }
+                else TailToHead();
+                
+            }
+            else if(index == head)
+            {
+                rect.anchoredPosition = new Vector2(0,0);
+            }
+            item[index%existCount].GetComponent<Selectable>().Select();
+        }
         void DealWithDown()
         {
             //如果能够显示的数量大于需要缓冲的，直接忽略
             // Debug.Log("before:"+head+","+tail+","+currentSelectedID);
+            if(currentSelectedID == count-1 )return ;
             if(maxCount>existCount)return;
             currentSelectedID ++;
             // Debug.Log("now:"+head+","+tail+","+currentSelectedID);
             int index = currentSelectedID;
+            
             if(index == tail - 1)
             {
                 if(maxCount == existCount)
@@ -66,6 +91,16 @@ namespace MyUI
             it.GetComponent<RectTransform>().SetAsLastSibling();
             head++;
             tail++;
+        }
+
+        void TailToHead()
+        {
+            int index = (tail-1)%existCount;
+            var it = item[index];
+            ButtonInitial(index,head - 1);
+            it.GetComponent<RectTransform>().SetAsFirstSibling();
+            head--;
+            tail--;
         }
         void ButtonInitial(int itemIndex,int dataIndex)
         {
@@ -147,7 +182,7 @@ namespace MyUI
             {   
                 Vector2 move = inputs.Player.Move.ReadValue<Vector2>();
                 
-                if (move.y > 0.1f) ;
+                if (move.y > 0.1f) DealWithUp();
                 else if (move.y < -0.1f) DealWithDown();
 
                 beginTime = Time.realtimeSinceStartup;
